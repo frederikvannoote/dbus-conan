72eedb4394b17954b21998cfeb11fb7be0db4072
Frederik Vannoote <frederik.vannoote@barco.com>
2021-11-13 18:06:22 +0100

    Adding conan recipe for dbus library

---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conans import ConanFile, CMake, tools
+from conan.tools.cmake import CMakeToolchain, CMake
+from conan.tools.layout import cmake_layout
+
+class DBusConan(ConanFile):
+    name = "dbus"
+    version = "1.13.18"
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt"
+
+    generators = "cmake_find_package"
+    
+    requires = (
+        "expat/2.4.1"
+    )
+    
+    def config_options(self):
+        self.options["expat"].shared = True
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def layout(self):
+        cmake_layout(self)
+        
+    def source(self):
+        git = tools.Git(folder=".")
+        git.clone("https://gitlab.freedesktop.org/dbus/dbus.git", "dbus-1.13.18")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = ["dbus"]
